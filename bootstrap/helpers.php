c501bd8caab4757b08d1cb4c394e5fb02765b543
luanguang <523086505@qq.com>
2019-05-09 21:18:24 +0800

    首页基础布局完成

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,3 +4,10 @@ function test_helper()
 {
     return "OK!";
 }
+
+if (!function_exists('route_class')) {
+    function route_class()
+    {
+        return str_replace('.', '-', Route::currentRouteName());
+    }
+}
