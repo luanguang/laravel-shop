fad74b2885f18f18380c7a4473a675cf9d96f13b
luanguang <523086505@qq.com>
2019-05-10 17:27:45 +0800

    收货地址添加,语言包问题没有解决

--- config/app.php
+++ config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'zh_HK',
 
     /*
     |--------------------------------------------------------------------------
@@ -158,6 +158,7 @@ return [
         Illuminate\Redis\RedisServiceProvider::class,
         Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
         Illuminate\Session\SessionServiceProvider::class,
+        // Overtrue\LaravelLang\TranslationServiceProvider::class,
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
